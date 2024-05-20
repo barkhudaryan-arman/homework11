2cd64283f8d05b5d49ce40292f407566cbbc9416
barkhudaryan-arman <armanik147@gmail.com>
2024-05-20 12:46:54 +0300

    Проверьте дз!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 2024;
+        year(a);
+    }
+    public static void year(int yearToCheck) {
+        if ((yearToCheck % 4 == 0 && yearToCheck % 100 != 0) || (yearToCheck % 400 == 0)) {
+            System.out.println("Год " + yearToCheck + " високосный");
+        } else {
+            System.out.println("Год " + yearToCheck + " не високосный");
+        }
+    }
+    public static void checkerDeviceClient(int deviceOS, int deviceYear) {
+        if (deviceYear < 0) {
+            System.out.println("Год не может быть меьше 0!");
+        }else{
+            int currentYear = LocalDate.now().getYear();
+            var type = deviceOS == 1 ? "Android" : "iOS";
+            var appType = deviceYear < currentYear ? "облегченную" : "";
+            System.out.println("Установите " + appType + " версию для системы " + type);
+        }
+    }
+    public static int calculateDistante(int distante){
+        if (distante > 100){
+            return -1;
+        }
+        int days = 1;
+        if (distante > 20){
+            days++;
+        }
+        if (distante > 60){
+            days++;
+        }
+        return days;
     }
 }
